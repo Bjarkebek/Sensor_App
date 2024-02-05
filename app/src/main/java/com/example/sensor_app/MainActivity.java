5969d6fabfcb0eb0227a37fb101f5a2d33f33a66
BjarkeBek <bjarke44@gmail.com>
2024-02-05 10:55:30 +0100

    added accelerator activity + page

--- app/src/main/java/com/example/sensor_app/MainActivity.java
+++ app/src/main/java/com/example/sensor_app/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.sensor_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,16 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager sensorManager;
-        ListView sensorView = findViewById(R.id.lv_sensor);
-        ArrayAdapter<String> adapter;
+        List<String> list = new ArrayList<>();
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        ((ListView) findViewById(R.id.lv_sensor)).setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list));
 
+        List<Sensor> deviceSensors = ((SensorManager) getSystemService(SENSOR_SERVICE))
+                .getSensorList(Sensor.TYPE_ALL);
+
+        // to add every sensor in the device to the list
+        for (Sensor sensor : deviceSensors) {
+            list.add(sensor.getName());
+        }
+
+        findViewById(R.id.btn_accelerator).setOnClickListener(view ->
+                startActivity(new Intent(this, AcceleratorActivity.class))
+        );
 
 
     }
-
-
 }
